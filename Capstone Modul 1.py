e4363399cdb26ba76a13a4e1331a830bdc3dcf7e
AliWafaar <141825588+AliWafaar@users.noreply.github.com>
2023-08-14 16:56:30 +0700

    Capstone Modul 1.py

--- Capstone Modul 1.py
+++ Capstone Modul 1.py
@@ -9,7 +9,7 @@ harga = [10000000, 8000000, 15000000]
 merek = ['Acer', 'Mi', 'Apple']
 
 # Membuat dictionary daftar_produk_dict dari data awal
-for i in range(len(sku)):
+for i in range(len(sku)): #Fungsi Loop untuk menambahkan Dict daftar product
     daftar_produk_dict[sku[i]] = {
         'nama': daftar_produk[i],
         'stock': stock[i],
@@ -17,23 +17,24 @@ for i in range(len(sku)):
         'merek': merek[i] 
     }
 
-# Fungsi untuk menampilkan daftar produk
+# Fungsi untuk menampilkan daftar produk dengan format harga yang menggunakan koma
 def tampilkan_daftar_produk():
     print("Daftar Produk:")
     print("SKU\t|Nama\t\t|Merek\t\t|Stock\t|Harga")
-    for key, value in daftar_produk_dict.items():
+    for key, value in daftar_produk_dict.items(): #Mengakses Dict daftar product
         formatted_harga = "{:,}".format(value['harga'])  # Format harga dengan koma
-        print(f"{key}\t|{value['nama']}\t\t|{value['merek']}\t\t|{value['stock']}\t|{formatted_harga}")
+        formatted_stock = "{:,}".format(value['stock'])  # Format stok dengan koma
+        print(f"{key}\t|{value['nama']}\t\t|{value['merek']}\t\t|{formatted_stock}\t|{formatted_harga}")
 
-# Fungsi untuk menambahkan produk baru
+# Fungsi untuk menambahkan produk
 def tambah_produk():
     produk_baru = input("Masukkan nama produk baru: ")
     merek_baru = input("Masukkan merek produk baru: ")
     stock_baru = int(input("Masukkan jumlah stok produk baru: "))
-    harga_baru = int(input("Masukkan harga produk baru (tanpa koma): "))
+    harga_baru = int(input("Masukkan harga produk baru (tanpa koma): ").replace(",", ""))  # Menghapus koma dari input harga
 
-    max_sku = max(daftar_produk_dict.keys())
-    new_sku = max_sku + 1
+    max_sku = max(daftar_produk_dict.keys()) #Mengakses kata kunci
+    new_sku = max_sku + 1 #Menambahkan keys yang baru
 
     daftar_produk_dict[new_sku] = {
         'nama': produk_baru,
@@ -91,16 +92,19 @@ def beli_produk():
         else:
             print("Nomor SKU produk tidak valid.")
 
-    print(f"Total yang harus dibayar = {total_harga}")
+    formatted_total_harga = "{:,}".format(total_harga)  # Format total harga dengan koma
+    print(f"Total yang harus dibayar = {formatted_total_harga}")
     while True:
         jumlah_uang = int(input("Masukkan jumlah uang = "))
         if jumlah_uang >= total_harga:
             uang_kembali = jumlah_uang - total_harga
-            print(f"Terimakasih Telah Berbelanja Uang kembali anda = {uang_kembali}")
+            formatted_uang_kembali = "{:,}".format(uang_kembali)  # Format uang kembali dengan koma
+            print(f"Terimakasih Telah Berbelanja Uang kembali anda = {formatted_uang_kembali}")
             break
         else:
             kurang_uang = total_harga - jumlah_uang
-            print(f'Uang Anda Kurang {kurang_uang}, silahkan masukkan jumlah yang cukup')
+            formatted_kurang_uang = "{:,}".format(kurang_uang)  # Format jumlah uang yang kurang dengan koma
+            print(f'Uang Anda Kurang {formatted_kurang_uang}, silahkan masukkan jumlah yang cukup')
 
 # Fungsi utama
 def main():
